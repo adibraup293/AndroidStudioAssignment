962e1926e4ae1865c3526909c8fa90d115dfe1dc
danieltoh16 <40682209+danieltoh16@users.noreply.github.com>
2019-11-21 17:24:26 +0800

    Updated: improved design and added back buttons

--- app/src/main/java/com/example/assignment/UserHomeActivity.java
+++ app/src/main/java/com/example/assignment/UserHomeActivity.java
@@ -23,6 +23,7 @@ public class UserHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
+        //test
 
     }
 }
