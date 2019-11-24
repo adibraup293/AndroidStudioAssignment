52d3a3345fd050cba804712d89d1253d8abbd3b5
danieltoh16 <40682209+danieltoh16@users.noreply.github.com>
2019-11-24 14:43:04 +0800

    Created layouts for allocate housing, view applications admin and view applications user

---
+++ app/src/main/java/com/example/assignment/Allocate/allocateHousing.java
@@ -0,0 +1,16 @@
+package com.example.assignment.Allocate;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.assignment.R;
+
+public class allocateHousing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_allocate_housing);
+    }
+}
---
+++ app/src/main/java/com/example/assignment/Application/ViewApplicationsUserActivity.java
@@ -0,0 +1,16 @@
+package com.example.assignment.Application;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.assignment.R;
+
+public class ViewApplicationsUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_applications_user);
+    }
+}
