5b52e608c9c2457690caf1b3a16e3737850832c5
danieltoh16 <40682209+danieltoh16@users.noreply.github.com>
2019-11-24 19:08:32 +0800

    Login

--- app/src/main/java/com/example/assignment/User/LoginActivity.java
+++ app/src/main/java/com/example/assignment/User/LoginActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,14 +20,42 @@ public class LoginActivity extends AppCompatActivity {
     EditText usernameEditText;
     EditText passwordEditText;
     DatabaseHelper databaseHelper;
-    Button loginButton;
+    Button loginButtonApplicant;
+    Button loginButtonAdmin;
+    RadioGroup radioGroup;
+    RadioButton applicantRB;
+    RadioButton adminRB;
 
     public void back(View view) {
         finish();
     }
 
+    public void LoginButtonAdmin(View view) {
 
-    public void LoginButton(View view){
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        //authenticate User
+        User currentUser = databaseHelper.loginUser(new User(username, password));
+
+        //check if authentication is successful or not
+        if (currentUser != null) {
+            Toast.makeText(this, "Logging in as " + username, Toast.LENGTH_SHORT).show();
+            //Officer Logged in successfully
+
+            Intent adminHomeIntent = new Intent(LoginActivity.this, AdminHomeActivity.class);
+            adminHomeIntent.putExtra("username", username);
+            startActivity(adminHomeIntent);
+            //
+
+        } else {
+            Toast.makeText(this, "No user found. Please create an account!", Toast.LENGTH_LONG).show();
+            //User Logged in failed.
+        }
+    }
+
+
+    public void LoginButtonApplicant(View view) {
 
         String username = usernameEditText.getText().toString();
         String password = passwordEditText.getText().toString();
@@ -34,15 +64,7 @@ public class LoginActivity extends AppCompatActivity {
         User currentUser = databaseHelper.loginUser( new User(username,password));
 
         //check if authentication is successful or not
-        if (currentUser.getUsertype() == 0) {
-            Toast.makeText(this, "Logging in as " + username, Toast.LENGTH_SHORT).show();
-            //Officer Logged in successfully
-
-            Intent adminHomeIntent = new Intent(LoginActivity.this, AdminHomeActivity.class);
-            adminHomeIntent.putExtra("username", username);
-            startActivity(adminHomeIntent);
-
-        } else if (currentUser.getUsertype() == 1) {
+        if (currentUser != null) {
             Toast.makeText(this, "Logging in as " + username, Toast.LENGTH_SHORT).show();
             //User Logged in successfully
 
@@ -67,10 +89,36 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        radioGroup = findViewById(R.id.radioGroupLogin);
+        applicantRB = findViewById(R.id.userBtn);
+        adminRB = findViewById(R.id.adminBtn);
+
         usernameEditText = findViewById(R.id.usernameEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
 
-        loginButton = findViewById(R.id.loginBtn);
+        loginButtonApplicant = findViewById(R.id.applicantLoginBtn);
+        loginButtonAdmin = findViewById(R.id.adminLoginBtn);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // checkedId is the RadioButton selected
+                applicantRB.isSelected();
+                if (checkedId == R.id.applicantBtn) {
+                    usernameEditText.setVisibility(View.VISIBLE);
+                    passwordEditText.setVisibility(View.VISIBLE);
+                    loginButtonApplicant.setVisibility(View.VISIBLE);
+                    loginButtonAdmin.setVisibility(View.INVISIBLE);
+
+                } else if (checkedId == R.id.adminBtn) {
+                    usernameEditText.setVisibility(View.VISIBLE);
+                    passwordEditText.setVisibility(View.VISIBLE);
+                    loginButtonApplicant.setVisibility(View.INVISIBLE);
+                    loginButtonAdmin.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
 
         databaseHelper = new DatabaseHelper(this);
     }
