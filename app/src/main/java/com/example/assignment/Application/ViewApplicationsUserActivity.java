f8f612f8c270c92903b1f3277341bddd062234a4
adibraup293 <36180086+adibraup293@users.noreply.github.com>
2019-11-25 14:58:38 +0800

    View Applications

--- app/src/main/java/com/example/assignment/Application/ViewApplicationsUserActivity.java
+++ app/src/main/java/com/example/assignment/Application/ViewApplicationsUserActivity.java
@@ -2,12 +2,16 @@ package com.example.assignment.Application;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.assignment.R;
 
 public class ViewApplicationsUserActivity extends AppCompatActivity {
+
+    ListView applicationsSubmitted;
+    Applications applications;
 
     public void back(View view) {
         finish();
